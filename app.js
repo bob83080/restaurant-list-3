d0f9f48a3222f6c1d5beb23ce1f98831b312b0e2
Bob Huo <bob83080@gmail.com>
2021-01-25 01:47:43 +0800

    edit and show page setup

--- app.js
+++ app.js
@@ -28,60 +28,63 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
-// app.get('/search', (req, res) => {
-//   const restaurants = restaurantList.results.filter(restaurant => { return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(req.query.keyword.toLowerCase()) })
-//   res.render('index', { restaurants: restaurants, keyword: req.query.keyword })
-// })
+app.get('/search', (req, res) => {
+  const restaurants = restaurantList.results.filter(restaurant => { return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(req.query.keyword.toLowerCase()) })
+  res.render('index', { restaurants: restaurants, keyword: req.query.keyword })
+})
 
-// app.get('/restaurants/:restaurant_id', (req, res) => {
-//   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-//   res.render('show', { restaurant: restaurant })
-// })
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
 
-// app.get('/create', (req, res) => res.render('create'))
+app.get('/create', (req, res) => res.render('create'))
 
-// app.post('/create/new', (req, res) => {
-//   if (req.body.image.length === 0) { req.body.image = 'https://www.teknozeka.com/wp-content/uploads/2020/03/wp-header-logo-33.png' }
-//   const restaurant = req.body
-//   return Restaurant.create(restaurant)
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
+app.post('/create/new', (req, res) => {
+  if (req.body.image.length === 0) { req.body.image = 'https://www.teknozeka.com/wp-content/uploads/2020/03/wp-header-logo-33.png' }
+  const restaurant = req.body
+  return Restaurant.create(restaurant)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
-// app.get('/:id/edit', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//     .lean()
-//     .then(restaurant => res.render('edit', restaurant))
-//     .catch(error => console.log(error))
-// })
+app.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', restaurant))
+    .catch(error => console.log(error))
+})
 
-// app.post('/:id/edit/update', (req, res) => {
-//   const id = req.params.id
-//   // const body = req.body
-//   return Restaurant.findById(id)
-//     .then(restaurant => {
-//       restaurant.name = req.body.name
-//       restaurant.category = req.body.category
-//       restaurant.image = req.body.image
-//       restaurant.loction = req.body.location
-//       restaurant.phone = req.body.phone
-//       restaurant.rating = req.body.rating
-//       restaurant.google_map = req.body.google_map
-//       restaurant.description = req.body.description
-//       return restaurant.save()
-//     })
-//     .then(() => res.redirect(`/restaurants/${id}`))
-//     .catch(error => console.log(error))
-// })
+app.post('/:id/edit/update', (req, res) => {
+  const id = req.params.id
+  // const body = req.body
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = req.body.name
+      restaurant.category = req.body.category
+      restaurant.image = req.body.image
+      restaurant.loction = req.body.location
+      restaurant.phone = req.body.phone
+      restaurant.rating = req.body.rating
+      restaurant.google_map = req.body.google_map
+      restaurant.description = req.body.description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
 
-// app.post('/:id/delete', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//     .then(restaurant => restaurant.remove())
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
+app.post('/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost: ${port}`)
